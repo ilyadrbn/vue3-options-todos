01e4daee6ee29412079f403bd5fd05689e8be697
ilyadrbn <ilyadrbn@gmail.com>
2024-04-25 09:47:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+import TheSection from '@/components/TheSection.vue'
+import TheTodoList from '@/components/TheTodoList.vue'
+import TheTodoInput from '@/components/TheTodoInput.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <TheSection />
+
+  <main class="app-main">
+    <TheTodoList />
+    <TheTodoInput />
+  </main>
+
+  <footer class="app-footer">2 more to do, 1 done</footer>
+</template>
+
+<style scoped></style>
